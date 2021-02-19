99e25504bb9447e86624b616333d12c17b048482
Rasf745 <79150355+Rasf745@users.noreply.github.com>
2021-02-18 23:42:10 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json")
+
+
+client.on("ready", () => {
+  console.log(`O bot foi iniciado, com ${client.users.cache.size} usuários e em ${client.guilds.cache.size} servidores.`);
+  client.user.setActivity('(O nome do status aqui)', { type: 'PLAYING' });(`Eu estou em ${client.guilds.cache.size} servidores`);
+});
+
+client.on("guildCreate", guild => {
+    console.log(`O bot entrou no servidor: ${guild.name} (ID do servidor: ${guild.id}). Membros: ${guild.memberCount} membros!`);
+    client.user.setActivity(`Estou em ${client.guilds.cache.size} servidores.`);
+});
+
+client.on("guildDelete", guild => {
+    console.log(`O bot foi removido do servidor: ${guild.name} (ID do servidor: ${guild.id})`);
+    client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
+});
+
+
+client.on("message", async message => {
+
+
+});
+
+client.login(config.token);
